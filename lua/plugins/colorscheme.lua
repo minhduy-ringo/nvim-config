9fe494d523ca9c7d44bcd9ae37c141731c61bd1c
ringo <minhduy.ringo@gmail.com>
2025-11-10 20:54:19 +0700

    Update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,13 +6,6 @@ return {
     config = function()
       require('catppuccin').setup {
 	transparent_background = false,
-	integrations = {
-	  dashboard = true,		  -- Highlight for dashboard.nvim
-	  snacks = {			  -- Snacks scope indent 
-	    enabled = true,
-	    indent_scope_color = 'flamingo',
-	  }
-	}
       }
       -- Load the theme
       vim.cmd([[colorscheme catppuccin]])
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -5,15 +5,6 @@ return {
     'akinsho/bufferline.nvim',
     version = '*',
     depedencies = { 'nvim-tree/nvim-web-devicons', 'catppuccin' },
-    config = function ()
-      require('bufferline').setup {
-	highlights = require('catppuccin.special.bufferline').get_theme(),
-	options = {
-	  themable = true,
-	  separator_style = 'slope',
-	},
-      }
-    end
   },
 
   -- Lua line
@@ -32,43 +23,5 @@ return {
 	}
       }
     end,
-  },
-
-  -- Neo tree
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons',
-      'MunifTanjim/nui.nvim',
-      'folke/snacks.nvim'
-    },
-    lazy = false,
-    ---@module 'neo-tree'
-    ---@type neotree.Config?
-    opts = {
-      -- NOTE: Add options here
-      close_if_last_window = true, -- Usually this mean exit Neovim, so just close it
-    },
-    keys = {
-      { '<leader>er', '<cmd>Neotree<CR>', desc = 'Explorer current' },
-      { '<leader>ed', function() 
-	  vim.ui.input(
-	    { prompt = 'Root dir to open Neotree' },
-	    function(path)
-	      local f = io.open(path, "r")
-	      if f then 
-		vim.cmd('Neotree' .. input)
-	      else
-		local snacks_notify = require('snacks.notify')
-		snacks_notify.error('Path not exist')
-	      end
-	    end
-	  )
-	end, desc = "Explorer dir"
-      },
-      { '<leader>et', '<cmd>Neotree toggle<CR>', desc = 'Explorer toggle' }
-    }
   },
 }
