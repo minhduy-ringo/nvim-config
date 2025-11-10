da58b205e48cf2ab5fb586923294d7d7d9b3d823
ringo <minhduy.ringo@gmail.com>
2025-11-10 21:55:49 +0700

    Add more colorschems

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,25 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    riority = 1000,
-    config = function()
-      require('catppuccin').setup {
-	transparent_background = false,
-      }
-      -- Load the theme
-      vim.cmd([[colorscheme catppuccin]])
-    end,
-  }
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        riority = 1000,
+        config = function()
+            require('catppuccin').setup {
+                transparent_background = false,
+            }
+            -- Load the theme
+            vim.cmd("colorscheme catppuccin")
+        end,
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end,
+    },
+    {
+        "EdenEast/nightfox.nvim",
+        name = "nightfox",
+    }
 }
